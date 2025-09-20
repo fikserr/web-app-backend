a9f14458e52b554b940eac7801e51fed0c221282
ADMIN <azizxon7777777@gmail.com>
2025-09-20 10:31:33 +0500

    add cors

---
+++ config/cors.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+'paths' => ['api/*', 'sanctum/csrf-cookie'],
+'allowed_methods' => ['*'],
+'allowed_origins' => ['http://localhost:5173','https://web-app-frontend-bice.vercel.app/', 'https://telegram-web-app-backend.laravel.cloud'],
+'allowed_origins_patterns' => [],
+'allowed_headers' => ['*'],
+'exposed_headers' => [],
+'max_age' => 0,
+'supports_credentials' => true,
+];
